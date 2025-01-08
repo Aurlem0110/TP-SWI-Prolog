35a76f3476d1d41016e13cd36f4f7cd8d8485759
Aurlem <lemr.aurelien@gmail.com>
2025-01-08 13:01:27 +0100

    Exercice 3

--- famille.pl
+++ famille.pl
@@ -16,3 +16,6 @@ pere(X, Y) :- homme(X), parent(X, Y).
 mere(X, Y) :- femme(X), parent(X, Y).
 
 grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+
+frere(X, Y) :- homme(X), parent(Z, X), parent(Z, Y).
+soeur(X, Y) :- femme(X), parent(Z, X), parent(Z, Y).
