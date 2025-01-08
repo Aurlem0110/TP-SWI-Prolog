56d346a8a5af8332357521d20916f724d96e3541
Aurlem <lemr.aurelien@gmail.com>
2025-01-08 12:02:33 +0100

    Exercice 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TP-SWI-Prolog
-# TP-SWI-Prolog
+
+__Ex1:__
+
+1) cf. 1
+
+2) cf. 2
--- famille.pl
+++ famille.pl
@@ -4,9 +4,13 @@ homme(paul).
 femme(marie).
 femme(sophie).
 
+homme(jacques).
+parent(jacques, marc).
+
 parent(pierre, paul).
 parent(marie, paul).
 parent(marc, sophie).
+
 
 pere(X, Y) :- homme(X), parent(X, Y).
 mere(X, Y) :- femme(X), parent(X, Y).
